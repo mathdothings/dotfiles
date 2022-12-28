42bbfb52815fbc9529ee0b5938b13ccb38754768
mathdothings <jhonata.prodrigues@gmail.com>
2022-12-27 21:41:43 -0300

    nvim config

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,18 @@
+vim.o.termguicolors = true
+vim.g.mapleader = ' '
+vim.g.mapLocalLeader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tabs
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
